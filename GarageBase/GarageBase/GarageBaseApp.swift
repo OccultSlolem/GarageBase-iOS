3f02cf241161be6f5ebc2d95051a1289a4d9bdd6
Ethan Hanlon <ehanlon19@gmail.com>
2021-06-21 23:22:39 -0700

    Add gitignore

---
+++ GarageBase/GarageBase/GarageBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarageBaseApp.swift
+//  GarageBase
+//
+//  Created by Ethan Hanlon on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct GarageBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
